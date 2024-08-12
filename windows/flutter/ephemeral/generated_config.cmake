328494e993127d90fc0804d518fbf9a728c393b7
HeyKanu <97739259+HeyKanu@users.noreply.github.com>
2024-08-12 13:26:33 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.19.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\kanu\\kanu\\projects\\chet_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.19.6-stable\\flutter"
+  "PROJECT_DIR=D:\\kanu\\kanu\\projects\\chet_app"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.19.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\kanu\\kanu\\projects\\chet_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\kanu\\kanu\\projects\\chet_app"
+  "FLUTTER_TARGET=D:\\kanu\\kanu\\projects\\chet_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\kanu\\kanu\\projects\\chet_app\\.dart_tool\\package_config.json"
+)
